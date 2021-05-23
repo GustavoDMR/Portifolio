e9877d599ebd4616c123c132cf00b4f626c9b6b5
Gustavin <gustavodemelorodrigues@gmail.com>
2021-05-22 23:38:38 -0300

    Add Exercicio 2

---
+++ Python/Geek University -  Conceitos/Seção 7 - Ex. 2.py
@@ -0,0 +1,19 @@
+"""'
+
+Exercicios da seção 7 do curso pt1
+
+Compreendendo indicies de listas
+
+"""
+
+# exercicio 2
+
+valores = []
+
+for valor in range(0, 2):
+
+    print(input('Insira 2 valores númerios para a lista: '))
+    valores.append(valor)
+
+print("A lista possui:", len(valores), "índices")
+
